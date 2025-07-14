598ba7a860098632d5e726381a5fae909b602f1d
Joao Romao <tupi_1800@hotmail.com>
2025-07-13 23:49:43 -0300

    show trending movies

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -31,15 +31,18 @@ const Search = () => {
       if (searchQuery.trim()) {
         await loadMovies();
         // call updateSearchCount only if there are results
-        if (movies?.length! > 0 && movies?.[0]) {
-          await updateSearchCount(searchQuery, movies[0]);
-        }
       } else {
         reset();
       }
     }, 750);
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
+
+  useEffect(() => {
+    if (movies?.length! > 0 && movies?.[0]) {
+      updateSearchCount(searchQuery, movies[0]);
+    }
+  }, [movies]);
 
   return (
     <View className="flex-1 bg-primary">
--- services/appWrite.ts
+++ services/appWrite.ts
@@ -42,3 +42,18 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
   //if no document is found:
   //create a new document in AppWrite database -> 1
 };
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log("Error right here: "+error);
+    return undefined;
+  }
+};
